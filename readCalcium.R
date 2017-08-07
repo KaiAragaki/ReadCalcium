30ffb205f6e0125f0876e0c96a87f257312c08c5
Kai Aragaki <7370516+KaiAragaki@users.noreply.github.com>
2017-08-07 09:46:27 -0700

    Update readCalcium.R
    Less code with same functionality and readability. Isn't that nice?

--- readCalcium.R
+++ readCalcium.R
@@ -3,7 +3,6 @@ readCalcium <- function(dateArg, ...) {
   # Initiate Variables
   conds <- c(..., "Ionomycin")
   divergeStart <- divergeEnd <- c()
-  cal340 <- cal380 <- baseline <- matrix()
   
   # Sets baseline and standard cond time intervals
   interval <- c(0:4)
@@ -12,18 +11,18 @@ readCalcium <- function(dateArg, ...) {
   
   # Loops for each cond and appends the time - not including Ionomycin time, which has its own interval.
   for (i in 1:(length(conds) - 1)){
-    addtlCond <- condInterval + tail(interval, n = 1)
+    addtlCond <- condInterval + last(interval)
     interval <- c(interval, addtlCond)
-    addTimes <- c(addTimes, tail(interval + 1, n = 1))
+    addTimes <- c(addTimes, last(interval) + 1)
   }
   
   # Tacks on ionomycin time
-  ionomycinInterval <- tail(interval, n = 1) + c(1.5, 2, 3, 4)
+  ionomycinInterval <- last(interval) + c(1.5, 2, 3, 4)
   condTimes <- c(interval, ionomycinInterval)
   
   # Reads and wrangles tab delimited file. If Lab folder position changes, this will break!
   path <- file.path("~", "Desktop", "Lab", "Experiments", "Calcium", "By Subject", "INS 832:3 Glucose Response", dateArg) %>%
-  setwd()
+    setwd()
   calFile <- read.delim(paste0(dateArg,"Results.xls"))
   calFile <- calFile[ ,-1]
   
@@ -31,66 +30,53 @@ readCalcium <- function(dateArg, ...) {
   minusBG <- mutate_all(calFile, function(x) {x - calFile[ ,1]})
   minusBG <- minusBG[ ,-1]
   
-  # Creates vectors of odd and even rows. Assumes 340 was first. Calculates ratio.
-  cal340 <- minusBG[seq(1, nrow(minusBG), 2),] # Want to make it so I can just put them in a dataframe right at the get go
-  cal380 <- minusBG[seq(2, nrow(minusBG), 2),]
-  calRatio <- cal340/cal380
+  # Slices and dices, with math
+  calDF <- data.frame("condTimes" = condTimes, "380" = stack(minusBG[seq(2, nrow(minusBG), 2),]), "340" = stack(cal340 <- minusBG[seq(1, nrow(minusBG), 2),])) %>%
+    mutate("340/380" = X340.values/X380.values) %>%
+    group_by(X380.ind) %>%
+    mutate("Baseline" = mean(`340/380`[1:5]), "maxIono" = max(tail(`340/380`)), "dynR" = (`340/380`-Baseline)/(maxIono-Baseline) * 100) %>%
+    group_by(condTimes) %>%
+    mutate("mean380" = mean(X380.values), "mean340" = mean(X340.values))
   
-  # Represents as % dynamic range
-  baseline <- colMeans(calRatio[1:5,])
-  maxIono <- apply(tail(calRatio), 2, function(x) max(x))
-  dynamicR <- sweep(sweep(calRatio, 2, baseline, "-"), 2, (maxIono - baseline), "/") * 100
-  
-  # Row means for finding slope means
-  rowMeans380 <- rowMeans(cal380)
-  rowMeans340 <- rowMeans(cal340)
-  
-  # Putting it all together...
-  calDF <- data.frame("Condition Times" = condTimes, "380" = stack(cal380), "340" = stack(cal340), "340/380" = stack(cal340/cal380), "340/380 Dyn" = stack(dynamicR))
- 
   # Loops through row means and checks to see if, on average, the slopes diverge.
   for (i in 1:(length(condTimes)-1)){
-    if(rowMeans380[i] - rowMeans380[i+1] > 50 & rowMeans340[i + 1] - rowMeans340[i] > 50  | rowMeans380[i+1] - rowMeans380[i] > 50 & rowMeans340[i] - rowMeans340[i + 1] > 50){
-      divergeStart[i] <- condTimes[i]
-      divergeEnd[i] <- condTimes[i+1]
+    if(calDF$mean380[i] - calDF$mean380[i+1] > 50 & calDF$mean340[i + 1] - calDF$mean340[i] > 50  | calDF$mean380[i+1] - calDF$mean380[i] > 50 & calDF$mean340[i] - calDF$mean340[i + 1] > 50){
+      divergeStart <- c(divergeStart, condTimes[i])
+      divergeEnd <- c(divergeEnd, condTimes[i+1])
     }
   }
-  divergeStart <- divergeStart[!is.na(divergeStart)]
-  divergeEnd <- divergeEnd[!is.na(divergeEnd)]
   rects <- data.frame(xstart = divergeStart, xend = divergeEnd)
   
-  # Plots -----------------------------------------------------
-  
-  # Template Theme and Geoms
+  # Template Plot
   calCommon <- list(theme(plot.title = element_text(hjust = 0.5), legend.position ="none", text = element_text(size = 20)), labs(x = "Time (min)", y = ""), geom_vline(xintercept = (addTimes), color = "red", alpha = 0.5), geom_point(), geom_line())
   
   # 380 Plot
-  ggplot(data = calDF, aes(x=Condition.Times, y=X380.values, color = X380.ind)) + 
+  ggplot(data = calDF, aes(x=condTimes, y=X380.values, color = X340.ind)) + 
     calCommon +
     labs(title = "380") + 
     annotate("text", x=addTimes, y=max(calDF$X380.values), label = conds, size=10)
   ggsave("380.pdf", device = "pdf", width = 16, height = 9, units = "in")
   
   # 340 Plot
-  ggplot(data = calDF, aes(x=Condition.Times, y=X340.values, color = X340.ind)) +
+  ggplot(data = calDF, aes(x=condTimes, y=X340.values, color = X340.ind)) +
     calCommon + 
     labs(title = "340") + 
     annotate("text", x=addTimes, y=max(calDF$X340.values), label = conds, size=10)
   ggsave("340.pdf", device = "pdf", width = 16, height = 9, units = "in")
   
   # 340/380 Plot
-  ggplot(data = calDF, aes(x=Condition.Times, y=X340.380.values, color = X340.380.ind)) + 
+  ggplot(data = calDF, aes(x=condTimes, y=`340/380`, color = X340.ind)) + 
     calCommon +
     labs(title = "340/380") + 
-    annotate("text", x=addTimes, y=max(calDF$X340.380.values), label = conds, size=10) +
+    annotate("text", x=addTimes, y=max(calDF$`340/380`), label = conds, size=10) +
     annotate("rect", xmin = rects$xstart, xmax = rects$xend, ymin = -Inf, ymax = Inf, alpha = 0.2, fill = "steelblue4")
   ggsave(paste("340\\380.pdf"), device = "pdf", width = 16, height = 9, units = "in")
   
   # 340/380 as % Dynamic Range Plot
-  ggplot(data = calDF, aes(x=Condition.Times, y=X340.380.Dyn.values, color = X340.380.Dyn.ind)) + 
+  ggplot(data = calDF, aes(x=condTimes, y=dynR, color = X340.ind)) + 
     calCommon +
     labs(title = "340/380 as % Dynamic Ratio") + 
-    annotate("text", x=addTimes, y=max(calDF$X340.380.Dyn.values), label = conds, size=10) +
+    annotate("text", x=addTimes, y=max(calDF$dynR), label = conds, size=10) +
     annotate("rect", xmin = rects$xstart, xmax = rects$xend, ymin = -Inf, ymax = Inf, alpha = 0.2, fill = "steelblue4")
   ggsave(paste("340\\380 As %% Dynamic Ratio.pdf"), device = "pdf", width = 16, height = 9, units = "in")
 }
